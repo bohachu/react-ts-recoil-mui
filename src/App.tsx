e36a3d4c5909ee9a646c9d0328bbebd7aae1e8ec
chiubowen <cbh@cameo.tw>
2023-03-09 21:50:09 +0800

    2023-03-09 21:49 改為 dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,24 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { RegisterPage } from './pages/RegisterPage';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </Router>    
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,15 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+
+
+const StyledGrid = styled(Grid)(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    padding: theme.spacing(2),
+  },
+}));
 
 function RegisterPage() {
   return (
-    <div>
-      <h1>Welcome to the RegisterPage</h1>
-      <Button component={Link} to="/" variant="contained">
-        Go to HomePage
-      </Button>
-    </div>
+      <StyledGrid container justifyContent="center" alignItems="center">
+        <Grid item xs={12} sm={6} md={4}>
+          <form>
+            <h1>Register</h1>
+            <TextField
+              required
+              fullWidth
+              margin="normal"
+              label="Username"
+              variant="outlined"
+            />
+            <TextField
+              required
+              fullWidth
+              margin="normal"
+              type="password"
+              label="Password"
+              variant="outlined"
+            />
+            <TextField
+              required
+              fullWidth
+              margin="normal"
+              type="email"
+              label="Email"
+              variant="outlined"
+            />
+            <Button fullWidth variant="contained" type="submit">
+              Register
+            </Button>
+          </form>
+          <Button component={Link} to="/" fullWidth>
+            Go to HomePage
+          </Button>
+        </Grid>
+      </StyledGrid>
   );
 }
 
